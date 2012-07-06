c8b5f791a46b373f83e48be092335110de4c7e0e
Simon Telling <simon@telling.nl>
2012-07-06 09:21:48 +0200

    Cosmetische verbetering aan het instellingen scherm.

--- admin.php
+++ admin.php
@@ -311,11 +311,11 @@ function fnInstellingen() {
 			}
 			printf("<tr><td class='label'>%s: </td><td><input type='checkbox' name='value_%d' value='1' %s></td><td>%s</td></tr>\n", $row->Naam, $row->RecordID, $c, $uitleg);
 		} elseif ($row->ParamType == "I") {
-			printf("<tr><td class='label'>%s: </td><td><input type='number' name='value_%d' value=%d size=8></td><td>%s</td></tr>\n", $row->Naam, $row->RecordID, $row->ValueNum, $uitleg);
+			printf("<tr><td class='label'>%s: </td><td><input type='number' class='inputnumber' name='value_%d' value=%d size=8></td><td>%s</td></tr>\n", $row->Naam, $row->RecordID, $row->ValueNum, $uitleg);
 		} elseif (strlen($row->ValueChar) > 60) {
-			printf("<tr><td class='label'>%s: </td><td><textarea cols=50 rows=7 name='value_%d'>%s</textarea></td><td>%s</td></tr>\n", $row->Naam, $row->RecordID, $row->ValueChar, $uitleg);
+			printf("<tr><td class='label'>%s: </td><td><textarea cols=45 rows=7 name='value_%d'>%s</textarea></td><td>%s</td></tr>\n", $row->Naam, $row->RecordID, $row->ValueChar, $uitleg);
 		} else {
-			printf("<tr><td class='label'>%s: </td><td><input type='text' name='value_%d' value=\"%s\" size=60></td><td>%s</td></tr>\n", $row->Naam, $row->RecordID, $row->ValueChar, $uitleg);
+			printf("<tr><td class='label'>%s: </td><td><input type='text' class='inputtext' name='value_%d' value=\"%s\"></td><td>%s</td></tr>\n", $row->Naam, $row->RecordID, $row->ValueChar, $uitleg);
 		}
 	}
 	echo("<tr><th colspan=3><input type='submit' value='Bewaren'></th></tr>\n");
