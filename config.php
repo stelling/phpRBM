585342d86fd2a46df9eda07fca917e969d60b6cf
Simon Telling <simon@telling.nl>
2012-07-19 01:12:28 +0200

    Standaard wachtwoord webmasters gelijk gemaakt aan het wachtwoord voor de database. Zo kan een webmaster inloggen, als de mailfunctie (nog) niet werken.

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 $db_host='';  // Server (host) naam van de MySQL-server.
 $db_name='';  // Gebruikersnaam voor de MYSQL-server
 $db_user='';  // Deze gebruiker moet onder andere rechten hebben om tabellen te mogen aanmaken en te verwijderen.
-$db_pass='';  // Wachtwoord wat bij de gebruikersnaam van MySQL hoort.
+$db_pass='';  // Wachtwoord wat bij de gebruikersnaam van MySQL hoort. Het standaard wachtwoord van webmasters is gelijk aan dit wachtwoord.
 $table_prefix = "rbm_"; // Zorg dat je bij het uploaden van de gegevens uit MS-Access dezelfde prefix gebruikt.
 $lididwebmasters = array(1);  // Dit is het interne nummer (RecordID) van het lid. Bij meerdere webmaster: scheiden met een komma.
 $encript_key = "deze_wijzigen"; // Deze wordt gebruikt om de encryptie van de wachtwoorden te doen. Wijzig deze na de eerste installatie niet meer. Als je dit toch doet zijn alle wachtwoorden gewijzigd en moet iedereen een nieuwe login aanvragen.
