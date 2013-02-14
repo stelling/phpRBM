9365f027c070710e752e7e3b7b7df4e1dbc0018d
Simon Telling <simon@telling.nl>
2013-02-14 01:01:48 +0100

    Opschonen overbodige code

--- auth.php
+++ auth.php
@@ -1,36 +1,24 @@
 <?php
 	include('./includes/standaard.inc');
-	
-	if (!isset($_GET['url']) or strlen($_GET['url']) == 0) {
-		$_GET['url'] = "/";
-	}
-	
-	$bewaartijdlogins = db_param("bewaartijdlogins");
-   
-	if (isset($_GET['actie']) and $_GET['actie'] == "uitloggen" and $_SESSION['lidid'] > 0) {
+
+	if (isset($_GET['actie']) and $_GET['actie'] == "uitloggen") {
 		db_logins("uitloggen", "", "", $_SESSION['lidid']);
 		session_destroy();
 		setcookie("username", "", time()-3600);
 		setcookie("password", "", time()-3600);
 		echo("<script>\nlocation.href='/';\n</script>\n");
-   } elseif ($_SESSION['lidid'] > 0) {
-		header ("Location: " . $_GET['url']);
    } elseif ($_SERVER['REQUEST_METHOD'] == "POST") {
 		$_SESSION['username'] = cleanlogin($_POST['username']);
       if (isset($_POST['cookie']) and $_POST['cookie'] == 1) {                                    
 			setcookie("username", $_SESSION['username'], time()+(3600*24*30));
 			setcookie("password", $_POST['password'], time()+(3600*24*30));
-      } else {
-			setcookie("username", "", time()-3600);
-			setcookie("password", "", time()-3600);
 		}
 		toegang("", 1, $_POST['password']);
 		unset($_SESSION['toegang']);
-		printf("<script>\nlocation.href='%s';\n</script>\n", $_SERVER['HTTP_REFERER']);
-   } else {
-		if (!isset($_SESSION['username']) and isset($_COOKIE['username'])) {
-			$_SESSION['username'] = cleanlogin($_COOKIE['username']);
+		if ($_SESSION['lidid'] > 0) {
+			printf("<script>\nlocation.href='%s';\n</script>\n", $_SERVER['HTTP_REFERER']);
+		} else {
+			printf("<p><a href='%s'>Klik hier om verder te gaan.</a></p>\n", $_SERVER['HTTP_REFERER']);
 		}
-		echo("<script>\nhistory.go(-1);\n</script>\n");
 	}
 ?>
