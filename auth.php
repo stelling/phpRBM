826b35bfbed080e15f90e42f356cf1ae464e6a82
Simon Telling <simon@telling.nl>
2013-02-18 13:13:04 +0100

    Wachtwoord alleen in een cookie gezet als hij ook gevuld is.

--- auth.php
+++ auth.php
@@ -11,12 +11,14 @@
 		$_SESSION['username'] = cleanlogin($_POST['username']);
       if (isset($_POST['cookie']) and $_POST['cookie'] == 1) {                                    
 			setcookie("username", $_SESSION['username'], time()+(3600*24*30));
-			setcookie("password", $_POST['password'], time()+(3600*24*30));
+			if (isset($_POST['password']) and strlen($_POST['password']) > 5) {
+				setcookie("password", $_POST['password'], time()+(3600*24*30));
+			}
 		}
-		toegang("", 1, $_POST['password']);
 		unset($_SESSION['toegang']);
+		toegang("", 1, $_POST['password']);
 		if ($_SESSION['lidid'] > 0) {
-			printf("<script>\nlocation.href='%s';\n</script>\n", $_SERVER['HTTP_REFERER']);
+			printf("<script>\nlocation.href='http://%s';\n</script>\n", $_SERVER["HTTP_HOST"]);
 		} else {
 			printf("<p><a href='%s'>Klik hier om verder te gaan.</a></p>\n", $_SERVER['HTTP_REFERER']);
 		}
