b279c696b9b66ff84fb2e20a04f9d832d36a6174
Simon Telling <simon@telling.nl>
2013-03-01 00:15:19 +0100

    Nettere afhandeling van inloggen zonder wachtwoord

--- auth.php
+++ auth.php
@@ -7,16 +7,21 @@
 		setcookie("username", "", time()-3600);
 		setcookie("password", "", time()-3600);
 		echo("<script>\nlocation.href='/';\n</script>\n");
-   } elseif ($_SERVER['REQUEST_METHOD'] == "POST") {
-		$_SESSION['username'] = cleanlogin($_POST['username']);
-      if (isset($_POST['cookie']) and $_POST['cookie'] == 1) {                                    
-			setcookie("username", $_SESSION['username'], time()+(3600*24*30));
-			if (isset($_POST['password']) and strlen($_POST['password']) > 5) {
-				setcookie("password", $_POST['password'], time()+(3600*24*30));
+	} elseif ($_SERVER['REQUEST_METHOD'] == "POST") {
+		if (strlen($_POST['password']) < 7) {
+			$mess = "Om in te loggen is het invullen van een wachtwoord van minimaal 7 karakters vereist.";
+			db_logboek("add", $mess, 1, 0, 2);
+		} else {
+			$_SESSION['username'] = cleanlogin($_POST['username']);
+			if (isset($_POST['cookie']) and $_POST['cookie'] == 1) {                                    
+				setcookie("username", $_SESSION['username'], time()+(3600*24*30));
+				if (isset($_POST['password']) and strlen($_POST['password']) > 5) {
+					setcookie("password", $_POST['password'], time()+(3600*24*30));
+				}
 			}
+			unset($_SESSION['toegang']);
+			toegang("", 1, $_POST['password']);
 		}
-		unset($_SESSION['toegang']);
-		toegang("", 1, $_POST['password']);
 		printf("<script>\nlocation.href='http://%s';\n</script>\n", $_SERVER["HTTP_HOST"]);
 	}
 ?>
