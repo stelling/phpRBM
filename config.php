6fe299e084fea090d00cf69c061b25f8c2343c53
Simon Telling <simon@telling.nl>
2018-07-10 00:01:27 +0200

    Eisen aan de encrypt_key zijn gewijzigd
    Er mogen nu alleen nog maar getallen in staan.

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $db_user=''; // Deze gebruiker moet onder andere rechten hebben om tabellen te m
 $db_pass=''; // Wachtwoord wat bij de gebruikersnaam van MySQL hoort. Het standaard wachtwoord van webmasters is gelijk aan dit wachtwoord.
 $table_prefix = "rbm_"; // Zorg dat je bij het uploaden van de gegevens uit MS-Access dezelfde prefix gebruikt.
 $lididwebmasters = array(1); // Dit is het interne nummer (RecordID) van het lid. Bij meerdere webmaster: scheiden met een komma.
-$encript_key = "deze_wijzigen"; // Deze wordt gebruikt om de encryptie van de wachtwoorden te doen. Wijzig deze na de eerste installatie niet meer. Als je dit toch doet zijn alle wachtwoorden gewijzigd en moet iedereen een nieuwe login aanvragen.
+$encript_key = "deze_wijzigen"; // Deze wordt gebruikt om de encryptie van de wachtwoorden te doen. Er mogen alleen getallen in staan en moet minimaal 3 karakters groot zijn. Wijzig deze na de eerste installatie niet meer. Als je dit toch doet zijn alle wachtwoorden gewijzigd en moet iedereen een nieuwe login aanvragen.
 
 $smtphost = ""; // De naam van de SMTP-server voor het versturen van e-mails. Indien deze niet wordt ingevuld, wordt van de mail-functie uit PHP gebruik gemaakt.
 $smtpport = 0; //De poort die voor de SMTP-host gebruikt moet worden. 0 = gebruik default poort.
