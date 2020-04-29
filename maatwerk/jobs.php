4b4ab1f23b51b0c8751de3cf9e3b36d217e28adb
Simon Telling <simon@telling.nl>
2020-04-29 13:14:19 +0200

    Update jobs.php

--- maatwerk/jobs.php
+++ maatwerk/jobs.php
@@ -5,6 +5,12 @@ require('../includes/standaard.inc');
 error_reporting(E_ALL);
 
 sentoutbox(1);
+if (date("i") <= 30) {
+	(new cls_login())->uitloggen();
+} else {
+	(new cls_login())->autounlock();
+}
+
 
 if (date("G") == 3) {
 	sleep(5);
