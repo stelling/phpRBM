b3f9846879d30c39ac65bc3e570384429d86a225
Simon Telling <simon@telling.nl>
2020-06-20 00:05:29 +0200

    Update jobs.php

--- maatwerk/jobs.php
+++ maatwerk/jobs.php
@@ -5,7 +5,9 @@ require('../includes/standaard.inc');
 error_reporting(E_ALL);
 
 if (date("G") == 0 or date("G") >= 7) {
-	sentoutbox(1);  // Versturen van de e-mails die staan te wachten in de outbox.
+	$res = sprintf("sentoutbox: %d verzonden", sentoutbox(1));  // Versturen van de e-mails die staan te wachten in de outbox.
+} else {
+	$res = "";
 }
 if (date("i") <= 30) {
 	(new cls_login())->uitloggen();  // Automatisch uitloggen van leden
@@ -13,12 +15,17 @@ if (date("i") <= 30) {
 	(new cls_login())->autounlock();  // Automatisch vrijgeven van geblokkeerde logins, dit op bases van de waarde in de instelling.
 }
 
+$lb = (new cls_Logboek())->max("DatumTijd", "TypeActiviteit=3");
 if ((date("G") == 0 or date("G") == 22) and date("i") <= 30) {
 	sleep(5);
-	(new cls_Lidond())->autogroepenbijwerken();  // Bijwerken van automatische groepen op basis van MySQL-code. 
-} elseif (date("G") == 3) {
+	$res .= sprintf(". %d automatische mutaties in leden van groepen", (new cls_Lidond())->autogroepenbijwerken());  // Automatisch bijwerken van groepen. 
+} elseif ($lb < date("Y-m-d H:i:s", mktime(date("H")-1, date("i"), 0, date("m"), date("d"), date("Y"))) and (date("G") == 3 or $lb < date("Y-m-d H:i:s", mktime(date("H")-25, date("i"), 0, date("m"), date("d"), date("Y")))))  {
 	sleep(5);
-	db_backup();  // Maken van een backup
+	if (db_backup()) {  // Maken van een backup
+		$res .= ". Backup is gemaakt";
+	}
 }
+
+(new cls_logboek())->add("jobs.php heeft gedraaid: " . $res, 2);
 
 ?>
