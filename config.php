6b621ac89a8333a3d7a7171e9c64cdb5d719269a
Simon Telling <simon@telling.nl>
2020-09-16 19:14:27 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $db_host=''; // Server (host) naam van de MariaDB-server.
 $db_name=''; // Gebruikersnaam voor MariaDB.
 $db_user=''; // Deze gebruiker moet onder andere rechten hebben om tabellen te mogen aanmaken en te verwijderen.
-$db_pass=''; // Wachtwoord wat bij de gebruikersnaam van MySQL hoort. Het standaard wachtwoord van webmasters is gelijk aan dit wachtwoord.
+$db_pass=''; // Wachtwoord wat bij de gebruikersnaam van MariaDB hoort. Het standaard wachtwoord van webmasters is gelijk aan dit wachtwoord.
 
 $table_prefix = "rbm_"; // Zorg dat je bij het uploaden van de gegevens uit MS-Access dezelfde prefix gebruikt.
 $lididwebmasters = array(0); // Dit zijn de interne nummers (RecordID's) van de webmasters. Bij meerdere webmaster: scheiden met een komma.
@@ -16,5 +16,12 @@ $smtpuser = ""; //De gebruiker om te kunnen inloggen op de SMTP-server.
 $smtppw = ""; //Het wachtwoord dat bij de SMTP-user hoort, om te kunnen inloggen op de SMTP-server.
 
 $httpsverplicht = 1; // Als waarde is 1 dan kan je alleen via https deze website bereiken.
+
+$lididwebmasters = array(0);
+
+$smtphost = "";
+$smtpport = 465;
+$smtpuser = "";
+$smtppw = "";
 
 ?>
