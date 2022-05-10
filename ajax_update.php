53dcc39d421282efce7dcb2441b2bb42f56c8c97
Simon Telling <simon@telling.nl>
2022-05-10 20:50:00 +0200

    Update ajax_update.php

--- ajax_update.php
+++ ajax_update.php
@@ -1,20 +1,30 @@
 <?php
 
-
 require_once('./includes/standaard.inc');
+toegang("", 0, 0);
 
-$ent = $_GET['entiteit'] ?? "";
-$id = $_POST['id'] ?? 0;
-$kolom = $_POST['field'] ?? "";
+if ($_SESSION['lidid'] > 0) {
 
-if ($ent == "lidedit") {
-	$i_dp = new cls_Diploma();
-	$i_dp->update($id, $kolom, $_POST['value']);
-	return true;
+	$ent = $_GET['entiteit'] ?? "";
+	$id = $_POST['id'] ?? 0;
+	$kolom = $_POST['field'] ?? "";
+
+	if ($ent === "lid") {
+		$i_lid = new cls_Lid();
+		$i_lid->update($id, $kolom, $_POST['value']);
+		
+	} elseif ($ent === "liddipl") {
+		$i_ld = new cls_Liddipl();
+		$i_ld->update($id, $kolom, $_POST['value']);
+		
+	} elseif ($ent === "lidond") {
+		$i_lo = new cls_Lidond();
+		$i_lo->update($id, $kolom, $_POST['value']);
 	
-} elseif ($ent == "diplomaedit") {
-	$i_dp = new cls_Diploma();
-	$i_dp->update($id, $kolom, $_POST['value']);
+	} elseif ($ent == "diplomaedit") {
+		$i_dp = new cls_Diploma();
+		$i_dp->update($id, $kolom, $_POST['value']);
+	}
 	return true;
 }
 
