dcdf0f51a67651818c6e5e4288f0bfc0b6c3a6fd
Simon Telling <simon@telling.nl>
2023-02-17 14:26:21 +0100

    Create pdf.php
    Code om het inschrijfformulier als PDF te tonen

---
+++ pdf.php
@@ -0,0 +1,31 @@
+<?php
+
+require('./includes/standaard.inc');
+
+$insid = $_GET['insid'] ?? 18;
+
+if ($_SESSION['lidid'] > 0) {
+	$i_ins = new cls_Inschrijving($insid);
+	
+	if ($i_ins->insid == 0) {
+		HTMLheader();
+		printf("<p class='mededeling'>Inschrijving %d bestaat niet.</p>\n", $insid);
+		
+	} elseif (strlen($i_ins->pdf()) == 0) {
+		HTMLheader();
+		printf("<p class='mededeling'>Aan inschrijving %d is geen PDF gekoppeld.</p>\n", $i_ins->insid);
+		
+	} else {
+
+		header("Content-type: application/pdf");
+		header("Content-Disposition: inline; filename=Inschrijfformulier $i_ins->naam $insid.pdf");
+	
+		echo($i_ins->pdf());
+	}
+	
+} else {
+	HTMLheader();
+	echo("<p class='mededeling'>Je moet zijn ingelogd om dit te kunnen bekijken.</p>\n");
+}
+
+?>
