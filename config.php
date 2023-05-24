62e32d6c59f2c08d9effb426147f00f7c4121ecc
Simon Telling <simon@telling.nl>
2023-05-24 18:17:27 +0200

    Update config.php
    Testers worden niet meer gebruikt

--- config.php
+++ config.php
@@ -7,7 +7,6 @@ $db_pass=''; // Wachtwoord wat bij de gebruikersnaam van MariaDB hoort. Het stan
 
 $table_prefix = "rbm_"; // Zorg dat je bij het uploaden van de gegevens uit MS-Access dezelfde prefix gebruikt.
 $lididwebmasters = array(0); // Dit zijn de interne nummers (RecordID's) van de webmasters. Bij meerdere webmaster: scheiden met een komma.
-$lididtestusers = array(0); // Dit zijn interne nummers (RecordID's) van het testgebruikers. Bij meerdere testgebruikers: scheiden met een komma.
 $salt2FA = ""; // Als je Two Factor Authentication wilt gebruiken vul dan hier een ongeveer 40 karakters lange code in.
 
 $smtphost = ""; // De naam van de SMTP-server voor het versturen van e-mails. Indien deze niet wordt ingevuld, wordt van de mail-functie uit PHP gebruik gemaakt.
