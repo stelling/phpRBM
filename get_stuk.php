567d16e23fe8ff42c9c396ff8e3cae0b9b478e47
Simon Telling <simon@telling.nl>
2023-05-29 17:45:21 +0200

    Create get_stuk.php

---
+++ get_stuk.php
@@ -0,0 +1,43 @@
+<?php
+require("./includes/standaard.inc");
+
+if (isset($_GET['p_stukid']) and $_GET['p_stukid'] > 0) {
+  
+	$i_st = new cls_Stukken($_GET['p_stukid']);
+	$i_lb = new cls_logboek();
+
+	if ($i_st->stid == 0) {
+		$mess = sprintf("Stuk %d bestaat niet.", $_GET['p_stukid']);
+		$i_lb->add($mess, 22, 0, 1, $i_st->stid, 8, "Stukken");
+	} elseif (substr($i_st->link, 0, 4) == "http") {
+		$mess = sprintf("Stuk %d is een externe download.", $i_st->stid);
+		$i_lb->add($mess, 22, 0, 1, $i_st->stid, 8, "Stukken");
+	} elseif ($i_st->magdownload) {
+		$filename = BASEDIR . "/stukken/" . $i_st->link;
+		if (file_exists($filename)) {
+			if (substr($i_st->link, -4) == ".pdf") {
+				header('Content-type: application/pdf');
+				header("Content-Length: " . filesize($filename));
+			} else {
+				header('Content-Type: application/zip');
+				header("Content-Disposition: attachment; filename={$i_st->link}" );
+			}
+			
+			header('Content-Transfer-Encoding: binary');
+			readfile($filename);
+			exit;
+		} else {
+			$mess = sprintf("Bestand '%s' bestaat niet.", $filename);
+			$i_lb->add($mess, 22, 0, 1, $i_st->stid, 8, "Stukken");
+		}
+	} else {
+		$mess = sprintf("Je bent niet gerechtigd om stuk %d te downloaden.", $i_st->stid);
+		$i_lb->add($mess, 22, 0, 1, $i_st->stid, 8, "Stukken");
+	}
+	
+} else {
+	$mess = "Het stuk is niet gespecificeerd.";
+	$i_lb->add($mess, 22, 0, 1, $i_st->stid, 8, "Stukken");
+}
+
+?>
