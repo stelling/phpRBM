679bcb4a317c619ed2cb33ff84dbc5502c8df1a9
Simon Telling <simon@telling.nl>
2023-11-16 18:04:39 +0100

    Update javascript.js
    Opschonen code

--- includes/javascript.js
+++ includes/javascript.js
@@ -1,3 +1,4 @@
+
 function fnFilter(p_table, p_filtercontrol, p_skipkolom=-1) {
 	var filter, table, tr, td, i, j, txtValue;
 	
@@ -29,76 +30,6 @@ function fnFilter(p_table, p_filtercontrol, p_skipkolom=-1) {
 	} else {
 		for (i = 0; i < tr.length; i++) {
 			tr[i].style.display = "";
-		}
-	}
-}
-
-function fnFilterTwee(p_table, p_input1, p_input2, p_col1, p_col2, p_input3, p_col3) {
-	var filter1, filter2, filter3, input1, input2, input3, table, tr, td1, td2, td3, i, txtValue1, txtValue2, txtValue3;
-	
-	if (p_col3 == undefined) {
-		p_col3 = -1;
-	}
-
-	input1 = document.getElementById(p_input1);
-	filter1 = input1.value.toUpperCase();
-	input2 = document.getElementById(p_input2);
-	filter2 = input2.value.toUpperCase();
-	if (p_col3 >= 0) {
-		input3 = document.getElementById(p_input3);
-		filter3 = input3.value.toUpperCase();
-	} else {
-		filter3 = "";
-	}
-	
-	table = document.getElementById(p_table);
-	tr = table.getElementsByTagName("tr");
-
-	for (i = 0; i < tr.length; i++) {
-		td1 = tr[i].getElementsByTagName("td")[p_col1];
-		td2 = tr[i].getElementsByTagName("td")[p_col2];
-		td3 = tr[i].getElementsByTagName("td")[p_col3];
-		if (td1) {
-			txtValue1 = td1.textContent || td1.innerText;
-			txtValue2 = td2.textContent || td2.innerText;
-			if (filter3.length > 0) {
-				txtValue3 = td3.textContent || td3.innerText;
-			} else {
-				txtValue3 = ""
-			}
-			if (filter1.length == 0 && filter2.length == 0 && filter3.length == 0) {
-				tr[i].style.display = "";
-			} else if (filter1.length > 0 && txtValue1.toUpperCase().indexOf(filter1) > -1) {
-				tr[i].style.display = "";
-			} else if (filter2.length > 0 && txtValue2.toUpperCase().indexOf(filter2) > -1) {
-				tr[i].style.display = "";
-			} else if (filter3.length > 0 && txtValue3.toUpperCase().indexOf(filter3) > -1) {
-				tr[i].style.display = "";
-			} else {
-				tr[i].style.display = "none";
-			}
-		}
-	}
-}
-
-function fnFilterDiplomaLid() {
-	var td1, td2, i, txtValue;
-	var input = document.getElementById("tbFilterCodeNaam");
-	var filter = input.value.toUpperCase();
-	var table = document.getElementById("diplomaslidmuteren");
-	var tr = table.getElementsByTagName("tr");
-
-	for (i = 0; i < tr.length; i++) {
-		td1 = tr[i].getElementsByTagName("td")[0];
-		td2 = tr[i].getElementsByTagName("td")[1];
-		if (td1) {
-			txtValue1 = td1.textContent || td1.innerText;
-			txtValue2 = td2.textContent || td2.innerText;
-			if (txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1) {
-				tr[i].style.display = "";
-			} else {
-				tr[i].style.display = "none";
-			}
 		}
 	}
 }
